a49ab08b0f525351cd9b259ef1facc3bc4536a3f
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2023-08-29 22:21:13 -0300

    fiz play button to destroy organisms and restart simulation after second click

--- app/public/Classes/Infos.js
+++ app/public/Classes/Infos.js
@@ -11,13 +11,13 @@ class Infos {
     }
 
     clear() {
-        this.populacao.length = 0
-        this.velocidade.length = 0
-        this.agilidade.length = 0
-        this.raio.length = 0
-        this.deteccao.length = 0
-        this.energia.length = 0
-        this.gasto.length = 0
-        this.tamanho_medio_ninhada.length = 0;
+        this.populacao.length = []
+        this.velocidade.length = []
+        this.agilidade.length = []
+        this.raio.length = []
+        this.deteccao.length = []
+        this.energia.length = []
+        this.gasto.length = []
+        this.tamanho_medio_ninhada.length = [];
     }
 }
