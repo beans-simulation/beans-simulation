aa107591308976fb3abdda6479b4d40ed165a9a0
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2023-09-05 03:31:03 -0300

    refactor history

---
+++ src/models/History.ts
@@ -0,0 +1,18 @@
+class History {
+  public organisms: Info;
+  public seconds: number[];
+  public vegetables_per_seconds: number[];
+
+  constructor() {
+    this.organisms = new Info();
+    this.seconds = [];
+    this.vegetables_per_seconds = [];
+  }
+
+  clear() {
+    this.organisms.clear();
+    this.seconds.length = 0;
+  }
+}
+
+export { History };
