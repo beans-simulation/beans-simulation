4c809246197488258ec23d413f97eda24adf79a3
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2023-09-13 03:04:23 -0300

    timer class and global timer

---
+++ src/models/timer.ts
@@ -0,0 +1,86 @@
+const MILLISECONDS_LIMIT = 1000;
+const SECONDS_LIMIT = 60;
+const MINUTES_LIMIT = 60;
+
+interface ITimer {
+  total: number;
+  hours: number;
+  seconds: number;
+  minutes: number;
+  milliseconds: number;
+  start: () => void;
+  reset: () => void;
+  pause: () => void;
+}
+
+export class Timer implements ITimer {
+  private time = 0;
+
+  private interval?: NodeJS.Timeout;
+  private interval_miliseconds = 10;
+
+  constructor() {
+    this.start();
+  }
+
+  private run(): void {
+    this.time += this.interval_miliseconds;
+  }
+
+  public start(): void {
+    this.interval = setInterval(this.run, this.interval_miliseconds);
+  }
+
+  // document.getElementById("hora").innerText = returnData(hora);
+  // document.getElementById("minuto").innerText = returnData(minuto);
+  // document.getElementById("segundo").innerText = returnData(segundo);
+  // document.getElementById("milisegundo").innerText = returnData(milisegundo);
+
+  public reset(): void {
+    this.time = 0;
+    clearInterval(this.interval);
+  }
+
+  public pause(): void {
+    clearInterval(this.interval);
+  }
+
+  private formatTime(time: number, min_length = 2): string {
+    return String(time).padStart(min_length, "0");
+  }
+
+  get formatted_timer(): string {
+    const hours = this.formatTime(this.hours);
+    const minutes = this.formatTime(this.minutes);
+    const seconds = this.formatTime(this.seconds);
+    const milliseconds = this.formatTime(this.milliseconds, 3);
+
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+  }
+
+  get hours(): number {
+    return Math.floor(
+      this.time / MINUTES_LIMIT / SECONDS_LIMIT / MILLISECONDS_LIMIT
+    );
+  }
+
+  get minutes(): number {
+    return Math.floor(
+      (this.time / SECONDS_LIMIT / MILLISECONDS_LIMIT) % MINUTES_LIMIT
+    );
+  }
+
+  get seconds(): number {
+    return Math.floor(
+      (this.time % (MINUTES_LIMIT * SECONDS_LIMIT)) / MILLISECONDS_LIMIT
+    );
+  }
+
+  get milliseconds(): number {
+    return this.time % MILLISECONDS_LIMIT;
+  }
+
+  get total(): number {
+    return this.time;
+  }
+}
--- src/resources/constants/globals.ts
+++ src/resources/constants/globals.ts
@@ -1,3 +1,10 @@
+import { Timer } from "@/models";
+
 export const magnitude_mutacao = 0.1; // magnitude da mutação (o quanto vai variar)
 
 export const probabilidade_mutacao = 0.3;
+
+export const global_timer = new Timer();
+
+export const universe_width = 1280;
+export const universe_height = 720;
