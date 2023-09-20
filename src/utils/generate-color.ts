a7590ba5dc820eb6ea6e98d614fe4c09d42fe1bf
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2023-09-20 20:25:03 -0300

    color functions: hex, rgb and generate colors

---
+++ src/utils/generate-color.ts
@@ -0,0 +1,10 @@
+function random_color() {
+  return Math.floor(Math.random() * 256);
+}
+
+export function generate_color() {
+  const r = random_color();
+  const g = random_color();
+  const b = random_color();
+  return `rgb(${r},${g},${b})`;
+}
---
+++ src/utils/rgb-to-hex.ts
@@ -0,0 +1,13 @@
+function format_color(color: string) {
+  return parseInt(color).toString(16).padStart(2, "0");
+}
+
+export function rgb_to_hex(rgb: string) {
+  const colors = rgb.match(/\d{1,3}/gi)?.map(format_color);
+
+  if (colors?.length === 3) {
+    const [r, g, b] = colors;
+    return "#" + r + g + b;
+  }
+  throw new Error(`Invalid RGB: ${rgb}`);
+}
