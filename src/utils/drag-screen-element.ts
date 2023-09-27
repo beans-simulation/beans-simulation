69373f0a26c2b6bcb6d0d976bf8e0219ae76a217
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2023-09-27 01:13:49 -0300

    fix drag elements event

--- src/utils/drag-screen-element.ts
+++ src/utils/drag-screen-element.ts
@@ -1,28 +1,33 @@
 function drag_mouse_down(event: MouseEvent) {
   event.preventDefault();
-  // get the mouse cursor position at startup:
-  document.onmouseup = close_drag_element;
-  // call a function whenever the cursor moves:
+  // when mouse move inside component
   document.onmousemove = element_drag;
+  // when user releases mouse button
+  document.onmouseup = stop_drag_element;
 }
 
 function element_drag(event: MouseEvent) {
   event.preventDefault();
   const element = event.target as HTMLDivElement | null;
+  const parent = element?.parentElement;
 
-  if (element) {
-    // calculate the new cursor position:
-    const x = event.clientX;
-    const y = event.clientY;
+  if (parent) {
+    const middleTabX = innerWidth - event.x - parent.clientWidth / 2;
+    const middleTabY = event.y - element.clientHeight / 2;
+
+    const minX = 5;
+    const maxY = innerHeight - parent.scrollHeight;
+
+    const x = middleTabX <= minX ? minX : middleTabX;
+    const y = middleTabY >= maxY ? maxY : middleTabY;
 
     // set the element's new position:
-    element.style.left = element.offsetLeft - x + "px";
-    element.style.top = element.offsetTop - y + "px";
+    parent.style.right = x + "px";
+    parent.style.top = y + "px";
   }
 }
 
-function close_drag_element() {
-  // stop moving when mouse button is released:
+function stop_drag_element() {
   document.onmouseup = null;
   document.onmousemove = null;
 }
