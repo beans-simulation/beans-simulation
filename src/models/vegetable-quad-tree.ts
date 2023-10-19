a49256a44a656307fbed1a80740f54784a1c7b5a
FShinoda <fernanda.shinoda@hotmail.com>
2023-10-19 13:58:20 -0300

    Splited the children classes from quadtree to 2 separeted models

---
+++ src/models/vegetable-quad-tree.ts
@@ -0,0 +1,20 @@
+class VegetableQuadTree extends QuadTree {
+    constructor(boundary: Rectangle, capacity: number){
+        super(boundary, capacity);
+    }
+
+    private is_vegetable(point: any): point is Vegetable {
+        return !!point?.vegetables;
+    }
+
+    search_elements(vision: Circle){
+        const found = super.search(vision);
+        // return found.filter(vegetable => {
+        //     if(is_vegetable(vegetable)){
+        //         return true
+        //     }
+        //     return false;
+        // })
+        return found;
+    }
+}
