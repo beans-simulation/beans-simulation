75cd09dcea0cc0e327c2fa22fdc704fe199679a7
Julia Rolemberg <julia.vicente@quintoandar.com.br>
2023-10-30 18:57:08 -0300

    refactor function is_organism to check existent attribute, and fix filter organisms on search

--- src/models/organism-quad-tree.ts
+++ src/models/organism-quad-tree.ts
@@ -1,19 +1,22 @@
 class OrganismQuadTree extends QuadTree {
     constructor(boundary: Rectangle, capacity: number){
-        super(boundary, capacity);        
+        super(boundary, capacity);
     }
-    
+
     private is_organism(point: any): point is Organism {
-        return !!point?.organisms;
+        if(point?.birth_moment_in_milliseconds !== undefined){
+            return true
+        }
+        return false
     }
 
     search_elements(vision: Circle, self_id?: number){
         const found = super.search(vision);
         return found.filter(organism => {
             if(this.is_organism(organism)){
-                organism.id !== self_id
+                return organism.id !== self_id
             }
             return false;
         })
-    } 
+    }
 }
