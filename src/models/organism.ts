29c20f2ee1bd71d67a18a74d5cc7bef422da5030
Pedro Heck <pedroscheck@hotmail.com>
2023-11-08 18:27:32 -0300

    Fixed variable calling error

--- src/models/organism.ts
+++ src/models/organism.ts
@@ -174,7 +174,7 @@ class Organism extends Point implements Drawable {
             console.log(i)
             let offspring_dna = this.crossover_dnas(current_organism_genome, partner_genome);
             const offspring_dna_mutated = offspring_dna.mutate();
-            child = this.create_child(offspring_dna_mutated);
+            const child = this.create_child(offspring_dna_mutated);
 
             // REPRODUÇÃO DAS REDES
             const this_nn_id = this.neural_network_id;
@@ -183,24 +183,26 @@ class Organism extends Point implements Drawable {
             const this_nn_id_JSON = JSON.stringify(this_nn_id);
             const partner_nn_id_JSON = JSON.stringify(partner_nn_id);
 
-            pyodide.runPython(`
-              import json
+            if (globals.pyodide){
+              globals.pyodide.runPython(`
+                import json
 
-              this_nn_id = json.loads('${this_nn_id_JSON}')
-              partner_nn_id = json.loads('${partner_nn_id_JSON}')
+                this_nn_id = json.loads('${this_nn_id_JSON}')
+                partner_nn_id = json.loads('${partner_nn_id_JSON}')
 
-              # Pegando as redes através de seus IDs
-              this_nn = neural_network.NeuralNetwork.neural_networks.get(f"{this_nn_id}")
-              partner_nn = neural_network.NeuralNetwork.neural_networks.get(f"{partner_nn_id}")
+                # Pegando as redes através de seus IDs
+                this_nn = neural_network.NeuralNetwork.neural_networks.get(f"{this_nn_id}")
+                partner_nn = neural_network.NeuralNetwork.neural_networks.get(f"{partner_nn_id}")
 
-              # Criando a rede filha
-              child_nn = breed_neural_networks(this_nn, partner_nn)
+                # Criando a rede filha
+                child_nn = breed_neural_networks(this_nn, partner_nn)
 
-              child_nn_id = child_nn.id
+                child_nn_id = child_nn.id
 
-            `);
+              `);
 
-            child.neural_network_id = pyodide.globals.get('child_nn_id');
+              child.neural_network_id = globals.pyodide.globals.get('child_nn_id');
+            }
           }
         }
         debugger;
