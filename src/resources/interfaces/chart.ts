74af8c87420a760c9e609f33aab362f10bdb9715
Raphael Menezes <raphael-mj@hotmail.com>
2023-11-12 17:39:47 -0300

    formated seconds for chart and interface

--- src/resources/interfaces/chart.ts
+++ src/resources/interfaces/chart.ts
@@ -1,1 +1,32 @@
 type ChartLabel = Pick<Plotly.Layout, "title" | "yaxis">;
+
+interface ChartDataByOrganism {
+  sum: {
+    detection_radius: number;
+    diet: number;
+    energy_consumption: number;
+    energy: number;
+    force: number;
+    lifetime: number;
+    maturity: number;
+    radius: number;
+    speed: number;
+  };
+  time: string;
+  number_of_organisms: number;
+}
+interface ChartData {
+  population: number;
+  time: string;
+  average: {
+    detection_radius: number;
+    diet: number;
+    energy_consumption: number;
+    energy: number;
+    force: number;
+    lifetime: number;
+    maturity: number;
+    size: number;
+    speed: number;
+  };
+}
