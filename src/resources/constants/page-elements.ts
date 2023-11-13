4bf420f221a15e0eb0a5de555f6b0822b22817c0
Raphael Menezes <raphael-mj@hotmail.com>
2023-11-12 21:59:26 -0300

    formating hmtl text and add resize icon button in chart tab

--- src/resources/constants/page-elements.ts
+++ src/resources/constants/page-elements.ts
@@ -9,6 +9,7 @@ const button_set_default = document.getElementById("button_set_default");
 const button_start_simulation = document.getElementById(
   "button_start_simulation"
 );
+const button_resize_chart = document.getElementById("button_resize_chart");
 
 // inputs iniciais
 const input_vegetable_rate = document.getElementById(
