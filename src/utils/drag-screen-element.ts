bc87fe2405b8045490a0f0f8c97f56f8358eca84
Raphael Menezes <raphael-mj@hotmail.com>
2023-11-13 01:24:12 -0300

    adjusting drag element after dynamic tab size

--- src/utils/drag-screen-element.ts
+++ src/utils/drag-screen-element.ts
@@ -1,33 +1,23 @@
-function drag_mouse_down(event: MouseEvent) {
-  event.preventDefault();
-  // when mouse move inside component
-  document.onmousemove = element_drag;
-  // when user releases mouse button
-  document.onmouseup = stop_drag_element;
-  // when mouse go outside of title
-  document.onmouseleave = stop_drag_element;
-}
+function get_new_x(parent: HTMLElement, element: HTMLElement, event_x: number) {
+  const middleTabX = innerWidth - event_x - parent.clientWidth / 2;
 
-function get_new_x(
-  minX: number,
-  maxX: number,
-  middleTabX: number,
-  eventX: number
-) {
-  const title_size = 300;
-  if (eventX - title_size / 2 <= 0) {
+  const minX = 5;
+  const maxX = innerWidth - parent.clientWidth - 5;
+
+  const title_size = element.clientWidth;
+  if (event_x - title_size / 2 <= 0) {
     return maxX;
   }
   return middleTabX <= minX ? minX : middleTabX;
 }
 
-function get_new_y(
-  minY: number,
-  maxY: number,
-  middleTabY: number,
-  event_y: number
-) {
-  const title_size = 60;
+function get_new_y(parent: HTMLElement, element: HTMLElement, event_y: number) {
+  const middleTabY = event_y - element.clientHeight / 2;
+
+  const minY = 5;
+  const maxY = innerHeight - parent.clientHeight - 5;
+
+  const title_size = element.clientHeight;
 
   if (event_y - title_size / 2 <= 0) {
     return minY;
@@ -40,20 +30,10 @@ function element_drag(event: MouseEvent) {
   event.preventDefault();
   const element = event.target as HTMLDivElement | null;
   const parent = element?.parentElement;
-  const tab = element?.closest(".tab-title");
 
-  if (element && parent && tab) {
-    const middleTabX = innerWidth - event.x - parent.clientWidth / 2;
-    const middleTabY = event.y - tab.clientHeight / 2;
-
-    const minX = 5;
-    const maxX = innerWidth - parent.clientWidth - 5;
-
-    const minY = 5;
-    const maxY = innerHeight - parent.clientHeight - 5;
-
-    const x = get_new_x(minX, maxX, middleTabX, event.x);
-    const y = get_new_y(minY, maxY, middleTabY, event.y);
+  if (element && parent) {
+    const x = get_new_x(parent, element, event.x);
+    const y = get_new_y(parent, element, event.y);
 
     // controlar a posicao atraves do right e top
     if (parent.style.left) parent.style.left = "";
@@ -67,8 +47,17 @@ function element_drag(event: MouseEvent) {
 function stop_drag_element() {
   document.onmouseup = null;
   document.onmousemove = null;
+  document.onmouseleave = null;
 }
 
 function drag_screen_element(element: Element) {
-  (element as HTMLDivElement).onmousedown = drag_mouse_down;
+  (element as HTMLDivElement).onmousedown = (event) => {
+    event.preventDefault();
+    // when mouse move inside component
+    document.onmousemove = element_drag;
+    // when user releases mouse button
+    document.onmouseup = stop_drag_element;
+    // when mouse go outside of title
+    element.addEventListener("mouseleave", stop_drag_element);
+  };
 }
